9265f9b8f236bb0e71b50e30a972edc206b0fed1
stanley091610 <754083@student.dusd.net>
2025-05-20 09:57:36 -0700

    I make a new scripts and make that the Game Over work also I almost finish with the button.

--- Unit5UserInterface/Assets/Scripts/GameManager.cs
+++ Unit5UserInterface/Assets/Scripts/GameManager.cs
@@ -7,14 +7,18 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
-    private float spawnRate = 1.0f;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
     private int score;
+    private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
+
+        StartCoroutine(SpawnTarget());
         UpdateScore(0);
     }
 
@@ -26,13 +30,11 @@ public class GameManager : MonoBehaviour
    
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
-
-            UpdateScore(5);
         }
     }
 
@@ -41,4 +43,10 @@ public class GameManager : MonoBehaviour
        score += scoreToAdd;
        scoreText.text = "Score: " + score;
    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
 }
--- Unit5UserInterface/Assets/Scripts/Target.cs
+++ Unit5UserInterface/Assets/Scripts/Target.cs
@@ -12,6 +12,9 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
 
+    public ParticleSystem explosionParticle;
+    public int pointValue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,16 +35,24 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 
-    Vector3 RandomForce()
+        Vector3 RandomForce()
         {
             return Vector3.up * Random.Range(minSpeed, maxSpeed);
         }
