eab80aa422dbcd25b3e60cfabd082a87b722e63c
stanley091610 <754083@student.dusd.net>
2025-05-27 09:56:47 -0700

    I finish my scripts and also I finish all the components

--- Unit5UserInterface/Assets/Scripts/GameManager.cs
+++ Unit5UserInterface/Assets/Scripts/GameManager.cs
@@ -10,11 +10,15 @@ public class GameManager : MonoBehaviour
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI livesText;
     public Button restartButton;
     public GameObject titleScreen;
+    public GameObject pauseScreen;
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
+    private int lives;
+    private bool paused;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +29,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Check if the user has pressed the P key
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
    
     IEnumerator SpawnTarget()
@@ -44,6 +52,16 @@ public class GameManager : MonoBehaviour
        scoreText.text = "Score: " + score;
    }
 
+    public void UpdateLives (int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
     public void GameOver()
     {
         restartButton.gameObject.SetActive(true);
@@ -58,13 +76,30 @@ public class GameManager : MonoBehaviour
 
     public void StartGame(int difficulty)
     {
+        spawnRate /= difficulty;
+
         isGameActive = true;
-        score = 0;
-        spawnRate = spawnRate / difficulty;
 
         StartCoroutine(SpawnTarget());
+        score = 0;
         UpdateScore(0);
-
+        UpdateLives(3);
         titleScreen.gameObject.SetActive(false);
+    }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
